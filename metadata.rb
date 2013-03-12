dca1157c4936d360264b88201e230e7ba9cdc90b
Josh Mervine <jmervine@yp.com>
2013-03-12 12:41:11 -0700

    Fixing template variable calls.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "joshua@mervine.net"
 license          "MIT"
 description      "Installs/Configures nginx_rails"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 depends          "nginx"
 
