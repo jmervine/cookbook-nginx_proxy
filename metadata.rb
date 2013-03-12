4325e3f52524346080c51e74fe9b5fe13a88be90
Josh Mervine <jmervine@yp.com>
2013-03-12 13:30:20 -0700

    Fixing metadata file typo

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "nginx_proxy"
 maintainer       "Joshua Mervine"
 maintainer_email "joshua@mervine.net"
 license          "MIT"
-description      "Installs/Configures nginx_rails"
+description      "Installs/Configures nginx_proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.3"
 depends          "nginx"
