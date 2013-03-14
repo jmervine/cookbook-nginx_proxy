be4e7948c7d4650031b0cf0ad7e227dfb14cd93f
Josh Mervine <jmervine@yp.com>
2013-03-13 21:53:21 -0700

    updating version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "joshua@mervine.net"
 license          "MIT"
 description      "Installs/Configures nginx_proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.8"
+version          "0.2.9"
 depends          "nginx"
 
